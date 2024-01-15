262125fb4d466abdef07b99446f2c195d451b466
IsGoose <47721300+IsGoose@users.noreply.github.com>
2024-01-15 19:00:09 +0000

    Update Configuration

--- Hive/Application/Configuration.cs
+++ Hive/Application/Configuration.cs
@@ -31,10 +31,7 @@ namespace Hive.Application
         
         [JsonProperty(Required = Required.Always)]
         public string[][] GameLogMap { get; set; }
-        
-        [JsonProperty(Required = Required.Always)]
-        public string[] LocalObjects { get; set; }
-        
+
         [JsonProperty(Required = Required.Always)]
         public string[] LocalObjectVectorChecks { get; set; }
     }
